fcd8dd52ad63d4f43f03c0a6a88fa8cc60fbd535
Alejo Agustin Martinez Aseijas <martinezalejo123a@gmail.com>
2021-01-20 11:09:44 -0300

    autoSave 21/01/21

---
+++ js/data.js
@@ -0,0 +1,99 @@
+const remeras = [{
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/1.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R001"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/2.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R002"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/3.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R003"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/4.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R004"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/5.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R005"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/6.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R006"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/7.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R007"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/8.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R008"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/9.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R009"
+    },
+    {
+        "name": "Remera Crepe Rayada",
+        "size": "1,2",
+        "price": "",
+        "img": "../img/remeras/10.jpg",
+        "stock": 4,
+        "available": true,
+        "id": "R010"
+    }];
+
+const pantalones = [
+    {
+
+    },
+    {
+
+    },
+];
